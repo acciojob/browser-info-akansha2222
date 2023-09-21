e36aef3954062a2ce443bbaac77434d5b7e2951f
akanshavrma <134785932+akansha2222@users.noreply.github.com>
2023-09-21 18:59:45 +0530

    created a submission

--- script.js
+++
@@ -1,4 +0,0 @@
-//your JS code here. If required.
-const text ="You are using " + navigator.appName + " version " + navigator.appVersion;
-console.log(text);
-document.getElementById("browser-info").innertext=text;
